53a272e44ab0f817581740178591b83b8acdb77c
Ax_Milin <151182569+AxMilin@users.noreply.github.com>
2024-02-12 17:34:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-import time
-from mcstatus import MinecraftServer
+import time, os
+from mcstatus import JavaServer
 
 def get_server_status(server_address, server_port):
-    server = MinecraftServer(server_address, server_port)
+    server = JavaServer.lookup(server_address+":"+server_port)
     status = server.status()
 
     return {
-        'latency': status.latency,
-        'uptime': time.time() - status.created,
+        'latency': round(status.latency),
         'online_players': status.players.online,
         'max_players': status.players.max
     }
@@ -15,15 +14,14 @@ def get_server_status(server_address, server_port):
 def display_server_status(server_address, server_port):
     while True:
         status = get_server_status(server_address, server_port)
-
+        os.system("cls||clear")
         print(f"Status: {'Online' if status['latency'] is not None else 'Offline'}")
         print(f"Latency: {status['latency']} ms")
-        print(f"Uptime: {status['uptime']:.2f} seconds")
         print(f"Players: {status['online_players']}/{status['max_players']}")
 
         time.sleep(10)
 
 server_address = input("Server Ip Address/Domain: ")
-server_port = int(input("Port: "))
+server_port = input("Port: ")
 
 display_server_status(server_address, server_port)
